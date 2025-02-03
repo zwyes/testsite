012924317f7be1b86faefa5dadafd97132cdba61
zw <197568559+zwyes@users.noreply.github.com>
2025-02-03 16:03:45 +0800

    typo lol

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let pushed = 0;
 
 
 
-button.addEventListener("click", function() {
+candyButton.addEventListener("click", function() {
     alert("Summoning candy!! Watch out!!");
     pushed += 1;
     candyButton.textContent = "Click me for more candy (" + toString(pushed) + ")";
